87f140b346bb59a4a6c073e9601c844bc4352a1c
JoanaOlivei <105799665+JoanaOlivei@users.noreply.github.com>
2024-02-15 21:25:50 +0000

    .

--- src/index.js
+++ src/index.js
@@ -136,7 +136,7 @@ function displayForecast(response) {
   const forecastData = response.data;
 
   if (forecastData && forecastData.daily) {
-    let forecastHtml = "<div class='row'>";
+    let forecastHtml = "<div class='row weather-forescast' >";
 
     forecastData.daily.slice(1, 6).forEach(function (day) {
       // Note que agora estamos usando .slice(1, 6) para obter os próximos 5 dias, excluindo o atual.
@@ -157,7 +157,7 @@ function displayForecast(response) {
     });
 
     forecastHtml += "</div>";
-    
+
     const forecastElement = document.querySelector("#forecast");
     forecastElement.innerHTML = forecastHtml;
   } else {
